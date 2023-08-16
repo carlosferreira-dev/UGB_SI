38b4fc7722d6549943d1d9830e75034c8df1d061
Carlos Eduardo Ferreira <mail@carlosferreira.dev>
2023-08-15 21:58:50 -0300

    .

---
+++ 4/N1/Programação 3/ConceitosBasicos/ConceitosBasicos/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Win32.SafeHandles;
+
+Console.WriteLine("Hello, World!");
+
+int num1 = 20;
+int num2 = 22;
+int soma = num1 + num2;
+Console.WriteLine($"A soma entre {num1} e {num2} é {soma}");
