9845edf6d551a4f6b702a6f171efaa9c447528d7
Carlos Eduardo Ferreira <mail@carlosferreira.dev>
2023-08-22 20:58:56 -0300

    .

--- 4/N1/Programação 3/ConceitosBasicos/ConceitosBasicos/Program.cs
+++ 4/N1/Programação 3/ConceitosBasicos/ConceitosBasicos/Program.cs
@@ -1,4 +1,8 @@
-﻿using ConceitosBasicos;
+﻿/*/
+ * Exercício 1
+/*/
+
+using ConceitosBasicos;
 
 int n1 = 20;
 int n2 = 11, result;
@@ -8,14 +12,16 @@ result = soma.Soma(n1, n2);
 Console.WriteLine($"A soma dos números {n1} + {n2} é {result}");
 
 /*/
- * 
+ * Exercício 2
 /*/
 
 double metros = 10.45;
 Console.WriteLine($"A medida de {metros} corresponde a {Conversor.MetrosMilimetros(metros)} milimetros");
 
 /*/
- * 
- * *
+ * Exercício 
 /*/
 
+double valor_atual = 500.50;
+int percentual = 10;
+Console.WriteLine($"Valor atual:{valor_atual} - Percentual de desconto: {percentual} - Valor do desconto: {(CalculaDesconto.Desconto(valor_atual, percentual))} - Valor final da mercadoria: {valor_atual - (CalculaDesconto.Desconto(valor_atual, percentual))}");
