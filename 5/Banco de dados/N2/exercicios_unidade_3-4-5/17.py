ff1489cc92204b1fd6edac7439fcdaae51a6c366
Carlos Eduardo Ferreira <mail@carlosferreira.dev>
2024-05-07 11:42:34 -0300

    db

---
+++ 5/Banco de dados/N2/exercicios_unidade_3-4-5/17.py
@@ -0,0 +1,15 @@
+import sys
+sys.dont_write_bytecode = True
+from db import *
+from tabulate import tabulate
+#17 - Obter a matrícula e o nome de todos os alunos do 1º ano de ENGENHARIA MECÂNICA 
+#que tiveram no 1º bimestre avaliação em  MATEMÁTICA I ou em FÍSICA I.
+conn = db_connect()
+cursor = conn.cursor()
+sql = """
+SELECT 
+"""
+cursor.execute(sql)
+rows = cursor.fetchall()
+print(tabulate(rows, headers=['Matrícula', 'Nome', 'Média'], tablefmt="fancy_grid", numalign="center", stralign="center"))
+cursor.close()
