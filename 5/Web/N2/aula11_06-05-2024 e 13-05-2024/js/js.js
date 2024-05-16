ccf52ad6b597bf5da582e66ff7c3a6e618174cbb
Carlos Eduardo Ferreira <mail@carlosferreira.dev>
2024-05-16 19:44:37 -0300

    .

--- 5/Web/N2/aula11_06-05-2024 e 13-05-2024/js/js.js
+++ 5/Web/N2/aula11_06-05-2024 e 13-05-2024/js/js.js
@@ -1,7 +1,54 @@
 function start() { // inicio start()
   $("#inicio").hide();
   $("#fundoGame").append("<div id='jogador' class='anima1'></div>");
-  $("#fundoGame").append("<div id='inimigo1'></div>");
+  $("#fundoGame").append("<div id='inimigo1' class='anima2'></div>");
   $("#fundoGame").append("<div id='inimigo2'></div>");
-  $("#fundoGame").append("<div id='amigo'></div>");
+  $("#fundoGame").append("<div id='amigo'class='anima3'></div>");
+
+// Principais variáveis do jogo
+var jogo = {};
+var TECLA = {
+  W: 87,
+  S: 83,
+  D: 68
+};
+
+// Verifica se o usuário pressionou alguma tecla
+jogo.pressionou = [];
+
+$(document).keydown(function(e) {
+  jogo.pressionou[e.which] = true;
+});
+$(document).keyup(function(e) {
+  jogo.pressionou[e.which] = false;
+});
+
+
+// Game Loop
+jogo.timer = setInterval(loop, 30);
+function loop() {
+  movefundo();
+  movejogador();
+} // Fim loop()
+
+// Função que movimenta o fundo do jogo
+function movefundo() {
+  esquerda = parseInt($("#fundoGame").css("background-position"));
+  $("#fundoGame").css("background-position", esquerda - 1);
+} // fim movefundo()
+
+// Função que movimenta o jogador
+function movejogador() {
+  if (jogo.pressionou[TECLA.W]) {
+    var topo = parseInt($("#jogador").css("top"));
+    $("#jogador").css("top", topo - 10);
+  }
+  if (jogo.pressionou[TECLA.S]) {
+    var topo = parseInt($("#jogador").css("top"));
+    $("#jogador").css("top", topo + 10);
+  }
+  if (jogo.pressionou[TECLA.D]) {
+    // Chama função Disparo
+  }
+} // fim movejogador()
 } // fim start()
